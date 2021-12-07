6ea7b218729fbe10ba3627e33e2318945d3a580a
[Cedric] <[0320987@student.rocvantwente.nl]>
2021-12-07 13:51:18 +0100

    Add project files.

---
+++ Models/Blog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VeiligProgrammerenBlog.Models
+{
+    public class Blog
+    {
+        public int BlogID { get; set; }
+        public int AuteurID { get; set; }
+        public string Titel { get; set; }
+        public string Afbeelding { get; set; }
+        public string Plaatsingsdatum { get; set; }
+        public string LaatsteUpdate { get; set; }
+        public string Inhoud { get; set; }
+        public string Beeld { get; set; }
+
+}
+}
